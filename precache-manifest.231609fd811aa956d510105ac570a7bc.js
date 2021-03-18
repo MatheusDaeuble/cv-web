ddbf31bed764067f5636da35bb0265c0d442f2a1
Matheus Daeuble <matheusdaeuble@gmail.com>
2021-03-18 00:51:47 -0300

    Updates

---
+++ precache-manifest.231609fd811aa956d510105ac570a7bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b01a523e0aa8f9324ca79ec6e02621",
+    "url": "/cv-web/index.html"
+  },
+  {
+    "revision": "385e6ec7c723c06c150c",
+    "url": "/cv-web/static/css/main.5c73d30f.chunk.css"
+  },
+  {
+    "revision": "3b1318923f2ea799eea4",
+    "url": "/cv-web/static/js/2.70eeea3f.chunk.js"
+  },
+  {
+    "revision": "2693bcb226720b495baad09989b5e9ac",
+    "url": "/cv-web/static/js/2.70eeea3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385e6ec7c723c06c150c",
+    "url": "/cv-web/static/js/main.4a393391.chunk.js"
+  },
+  {
+    "revision": "2cccaa502591e6797a72",
+    "url": "/cv-web/static/js/runtime-main.1a6e3b9d.js"
+  },
+  {
+    "revision": "8f608bb0df4c3cd0c3a453f04d6a72a9",
+    "url": "/cv-web/static/media/avatar.8f608bb0.jpeg"
+  },
+  {
+    "revision": "77a7ff4692f3657d3a9378aaea96b8fc",
+    "url": "/cv-web/static/media/bg.77a7ff46.png"
+  }
+]);
